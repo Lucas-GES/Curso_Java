559f0ab6a33d740518ed01ab525e5e4df866866e
Lucas Gabriel <lucas.espiritosanto@edu.unifil.br>
2022-02-07 07:58:53 -0300

    Date, Date Calendar e exercicio de matriz

---
+++ Exercicio_Matriz/src/Program.java
@@ -0,0 +1,46 @@
+package src;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+
+        for( int i = 0; i< m; i++){
+            for( int j=0; j< n; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+
+        for (int i=0; i<m ; i++){
+            for(int j=0; j<n ; j++){
+                if(matrix[i][j] == x){
+                    System.out.println("Position " + i + "," + j + ":");
+                    if(j > 0) {
+                        System.out.println("Left: " + matrix[i][j - 1]);
+                    }
+                    if(j < matrix[i].length-1){
+                        System.out.println("Right: " + matrix[i][j + 1]);
+                    }
+                    if(i > 0) {
+                        System.out.println("Up: " + matrix[i - 1][j]);
+                    }
+                    if(i < matrix.length-1){
+                        System.out.println("Down: " + matrix[i + 1][j]);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
