b922ed66528629f1868b721f1f8093ec30dafe49
Lucas Gabriel <lucas.espiritosanto@edu.unifil.br>
2022-02-07 12:02:46 -0300

    Composicao, Composicao com StringBuilder, Enumeradores, Exercicio de Composicao, Heranca e Sobreposicao

---
+++ Enumeradores/src/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package src.entities.enums;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
---
+++ Exercicio_Composicao/src/entities/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package src.entities.enums;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+
+}
